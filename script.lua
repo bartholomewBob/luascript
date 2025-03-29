b743e34fdb37fb8b92a3b9299f43aa3c99ed95d9
bartholomewBob <105937421+bartholomewBob@users.noreply.github.com>
2025-03-29 16:13:38 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,79 +1,81 @@
--- Make tween infinite in runservice and set to active rock index
--- Add noclip to prevent damage
-
-print('ITERATION0: Started script')
-local player = game.Players.LocalPlayer
-local character = player.Character or player.CharacterAdded:Wait()
-local backpack = player:WaitForChild('Backpack')
-local humanoid = character:FindFirstChildOfClass('Humanoid')
-
+-- Get player character
 function get_character()
 	local player = game.Players.LocalPlayer
-	local _character = player.Character or player.CharacterAdded:Wait()
-	return _character
+	local character = player.Character or player.CharacterAdded:Wait()
+	return character
 end
 
+-- Get rock mesh (Position, CFrame)
 function get_mesh(rock)
 	return rock:FindFirstChildOfClass('MeshPart')
 end
 
 -- Cycle through all weapons, and use all skills
-function attack(position) 	
+function attack(position) 		
 	local weapons = {}
-	local _character
-
-	-- TEST THIS
-	pcall(function() _character = get_character() end)
-
-	if _character == nil then
+	
+	local character
+	pcall(function() character = get_character() end)
+	if character == nil then
+		print('ATTACK: Failed to find Character')
 		return
 	end
 
-	local _humanoid = _character:FindFirstChildOfClass('Humanoid')
-	
-	local _backpack = game.Players.LocalPlayer:WaitForChild('Backpack')
-	
+	local humanoid = character:FindFirstChildOfClass('Humanoid')		
+	if not humanoid then
+		print('ATTACK: Failed to find Humanoid')
+		return
+	end
+
+	local backpack = game.Players.LocalPlayer:findFirstChild('Backpack')
+	if not backpack then
+		print('ATTACK: Failed to find Backpack')
+		return
+	end
+
 	-- Get all weapons from player's backpack
-	print('ROCK: Fetching weapons...')
-	for _, weapon in _backpack:GetChildren() do
+	local tools = backpack:GetChildren()
+	
+	print('ATTACK: Fetching weapons from ' .. #tools .. ' children...')
+	for _, weapon in tools do
 		if weapon:FindFirstChild('EquipEvent') then
 			table.insert(weapons, weapon)
 		end
 	end
-	print('ROCK: Fetched ' .. #weapons .. ' weapon(s)')
+	print('ATTACK: Fetched ' .. #weapons .. ' weapon(s)')
 
 
 	-- Equip each weapon and use abilities
 	for index, weapon in weapons do
-		print('ROCK: Equipping weapon ' .. weapon.Name)
+		print('ATTACK: Equipping weapon ' .. weapon.Name)
 
-		_humanoid:EquipTool(weapon)
+		humanoid:EquipTool(weapon)
 		wait(0.5)
 
-		print('ROCK: Using abilities of weapon...')
+		print('ATTACK: Using abilities of weapon...')
 
 		for i, ability in { "Z", "X", "C", "V"} do
-			print('ROCK: Using ability "' .. ability .. '"...')
+			print('ATTACK: Using ability "' .. ability .. '"...')
 
 			-- Fire client side
 			game:GetService("ReplicatedStorage"):WaitForChild("Events", 9e9):WaitForChild("ActivatedSkill", 9e9):Fire(unpack({ [1] = ability }))
 
 
 			-- Get all remote functions for weapon skills
-			print('ROCK: Fetching remote functions...')
+			print('ATTACK: Fetching remote functions...')
 			local remotes = {}
-			for _, child in _humanoid:GetChildren() do			
+			for _, child in humanoid:GetChildren() do			
 				if child:IsA('RemoteFunction') and #remotes < #weapons then
 					table.insert(remotes, child)
 				end
 			end
-			print('ROCK: Fetched ' .. #remotes .. ' remote function(s)')
+			print('ATTACK: Fetched ' .. #remotes .. ' remote function(s)')
 
 			-- Iterate through each remote and invoke
 			for i, remote in remotes do
-				print('ROCK: Running remote ' .. i .. '...')
+				print('ATTACK: Running remote ' .. i .. '...')
 				local status, err = pcall(function() remote:InvokeServer(unpack({ [1] = ability, [2] = position})) end)
-				print('ROCK: Ran remote ' .. i .. ' with result ' .. tostring(status) .. ' (' .. tostring(err) .. ')')
+				print('ATTACK: Ran remote ' .. i .. ' with result ' .. tostring(status) .. ' (' .. tostring(err) .. ')')
 			end				
 		end
 
@@ -90,7 +92,7 @@ function get_rocks()
 		-- Check if Glow exists
 		local glow
 		pcall(function() glow = rock["VFXLayer"]["At0"]["Glow"] end)
-				
+
 		print('ROCK' .. tostring(index) .. ': Found Rock')
 		-- Check if Glow is enabled
 		if glow then
@@ -107,17 +109,17 @@ function get_rocks()
 end
 
 -- Tween to part 
-function tween_to(part) 
-	local _character
-
-	-- TEST THIS
-	pcall(function() _character = get_character() end)
-
-	if _character == nil then
-		return
-	end
+function tween_to(part, beat) 
+	local character
+	pcall(function() character = get_character() end)
+	if character == nil then
+		if beat then
+			print('TWEEN: Failed to find character')	
+		end		
+		return	
+	end		
 	
-	local dist = (part.Position - _character.HumanoidRootPart.Position).Magnitude
+	local dist = (part.Position - character.HumanoidRootPart.Position).Magnitude
 	local speed = 0
 
 	if dist < 150 then
@@ -135,7 +137,7 @@ function tween_to(part)
 	end
 
 	game:GetService("TweenService"):Create(
-		_character.HumanoidRootPart,
+		character.HumanoidRootPart,
 		TweenInfo.new(dist/speed, Enum.EasingStyle.Linear),
 		{CFrame = part.CFrame}
 	):Play()
@@ -167,23 +169,56 @@ function get_singular_rock(rock_index, rocks)
 	return rock
 end
 
--- Main loop
-local iterations = 0
+function is_beat(time) 
+	return math.fmod(time, 2) <= 0.02
+end
+
+
+-- Main loops
 local active_rock = -1
 
+local run_iterations = 0
+game:GetService("RunService").Stepped:Connect(function(time)
+	-- Get active rock if exists and tween to each frame
+	if game:GetService('Workspace').Map:FindFirstChild('PrehistoricIsland') then
+		local rocks = get_rocks()
+		if is_beat(time) then print('RUN-ITERATION' .. tostring(run_iterations) .. ': Found Prehistoric Island with ' .. tostring(#rocks) .. ' rock(s)') end		
+
+		if active_rock ~= -1 then
+			local singular_rock = get_singular_rock(active_rock, rocks)
+			local mesh = get_mesh(singular_rock)
+
+			if mesh then
+				if is_beat(time) then print('RUN-ITERATION' .. tostring(run_iterations) .. ': Tweening to active rock ' .. tostring(active_rock) .. '...') end 	
+			else
+				if is_beat(time) then print('RUN-ITERATION' .. tostring(run_iterations) .. ': Failed to find active rock ' .. tostring(active_rock) .. ' mesh') end
+			end
+
+			tween_to(mesh, is_beat(time))	
+		end
+	else
+		if is_beat(time) then print('RUN-ITERATION' .. tostring(run_iterations) .. ': Failed to find Prehistoric Island') end
+	end
+
+	if is_beat(time) then run_iterations += 1 end
+end)
+
+
+-- Main loop
+local while_iterations = 0
 while true do
 	-- Check if Prehistoric Island exists
 	if game:GetService('Workspace').Map:FindFirstChild('PrehistoricIsland') then
 		-- Get table of glowing rocks
 		local rocks = get_rocks()	
-	
-		print('ITERATION' .. tostring(iterations) .. ': Found ' .. tostring(#rocks) .. ' glowing rock(s)...')
+
+		print('WHILE-ITERATION' .. tostring(while_iterations) .. ': Found ' .. tostring(#rocks) .. ' glowing rock(s)...')
 
 		-- Check if a glowing rock is actively being attacked
 		if active_rock ~= -1 then
 			-- Check if rock is still glowing
 			if is_glowing(active_rock, rocks) then
-				print('ITERATION' .. tostring(iterations) .. ': Attacking active rock ' .. tostring(active_rock) .. '...')
+				print('WHILE-ITERATION' .. tostring(while_iterations) .. ': Attacking active rock ' .. tostring(active_rock) .. '...')
 
 				local singular_rock = get_singular_rock(active_rock, rocks)
 				local mesh = get_mesh(singular_rock)
@@ -194,38 +229,26 @@ while true do
 				if #rocks ~= 0 then
 					-- Then, move on to next rock
 					active_rock = rocks[1].index					
-					print('ITERATION' .. tostring(iterations) .. ': Moving on to rock ' .. tostring(active_rock) .. '...')
+					print('WHILE-ITERATION' .. tostring(while_iterations) .. ': Moving on to rock ' .. tostring(active_rock) .. '...')
 				else 
 					-- Otherwise, set active rock to none
 					active_rock = -1
-					print('ITERATION' .. tostring(iterations) .. ': Reset rocks...')
+					print('WHILE-ITERATION' .. tostring(while_iterations) .. ': Reset rocks...')
 				end
 			end
 		else
 			if #rocks ~= 0 then
 				active_rock = rocks[1].index
-				print('ITERATION' .. tostring(iterations) .. ': Starting with rock ' .. tostring(active_rock) .. '...')
+				print('WHILE-ITERATION' .. tostring(while_iterations) .. ': Starting with rock ' .. tostring(active_rock) .. '...')
 			end
 		end
 	else
-		print('ITERATION' .. tostring(iterations) .. ': Failed to find Prehistoric Island...')
+		print('WHILE-ITERATION' .. tostring(while_iterations) .. ': Failed to find Prehistoric Island...')
 		active_rock = -1
 	end
 
-	iterations += 1
+	while_iterations += 1
 	wait(1)
 end
 
-game:GetService("RunService").Heartbeat:Connect(function()
-	-- Get active rock if exists and tween to each frame
-	if game:GetService('Workspace').Map:FindFirstChild('PrehistoricIsland') then		
-		local rocks = get_rocks()
 
-		if active_rock ~= -1 then
-			local singular_rock = get_singular_rock(active_rock, rocks)
-			local mesh = get_mesh(singular_rock)
-
-			tween_to(mesh)
-		end
-	end
-end)
